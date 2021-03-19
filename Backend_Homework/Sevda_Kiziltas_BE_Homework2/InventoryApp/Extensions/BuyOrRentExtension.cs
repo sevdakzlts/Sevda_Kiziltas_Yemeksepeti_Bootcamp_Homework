a65fe64dabade750faa9f855f48675a4d527e279
sevdakzlts <65716312+sevdakzlts@users.noreply.github.com>
2021-03-19 13:08:35 +0300

    initial commit

---
+++ Backend_Homework/Sevda_Kiziltas_BE_Homework2/InventoryApp/Extensions/BuyOrRentExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryApp.Model;
+
+namespace InventoryApp.Extensions
+{
+    public static class BuyOrRentExtension
+    {
+        public static string BuyOrRent(this InventoryItem item)
+        {
+            return item.RentPrice * item.PeriodType * item.UsagePeriod  >= item.Price ? "You should buy" : "You should rent";
+        }
+    }
+}
---
+++ Backend_Homework/Sevda_Kiziltas_BE_Homework2/InventoryApp/Validation/StringLengthValidator.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+using InventoryApp.Controllers;
+using ValidationResult = FluentValidation.Results.ValidationResult;
+
+namespace InventoryApp.Validation
+{
+    public class StringLengthValidator : ValidationAttribute
+    {
+        private int _minLength;
+
+        public StringLengthValidator(int minLength)
+        {
+            _minLength = minLength;
+        }
+
+        protected override System.ComponentModel.DataAnnotations.ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            InventoryController.SearchModel searchModel =
+                (InventoryController.SearchModel) validationContext.ObjectInstance;
+            if (searchModel.name.Length < _minLength)
+            {
+                return new System.ComponentModel.DataAnnotations.ValidationResult(FormatErrorMessage(validationContext.MemberName));
+            }
+
+            return System.ComponentModel.DataAnnotations.ValidationResult.Success;
+        }
+
+
+       
+    }
+}
